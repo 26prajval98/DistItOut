5f3eca2007474bbbc6ba668091ebb50d878a556c
26prajval98 <26prajval98@gmail.com>
2020-07-29 18:15:44 +0530

    Updates

---
+++ precache-manifest.2a57d0c35f4a605ba4516d4fbeabd0f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e90a0cdc81c0c5349f7b944ed8a2de7f",
+    "url": "/static/media/shop.e90a0cdc.jpg"
+  },
+  {
+    "revision": "aa069dd7781d1c96a6a2772eb87c7f99",
+    "url": "/static/media/recreation.aa069dd7.jpg"
+  },
+  {
+    "revision": "1edf3b22d4e016931862af697d79052e",
+    "url": "/static/media/eat.1edf3b22.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8aae282bd8e44fb7c48f",
+    "url": "/static/js/main.3eb5b77f.chunk.js"
+  },
+  {
+    "revision": "6ed151fff1dccfe032c9",
+    "url": "/static/js/2.a6327a38.chunk.js"
+  },
+  {
+    "revision": "8aae282bd8e44fb7c48f",
+    "url": "/static/css/main.d2b43652.chunk.css"
+  },
+  {
+    "revision": "5926fc600454025f1dcff8ac47f025b8",
+    "url": "/index.html"
+  }
+];
