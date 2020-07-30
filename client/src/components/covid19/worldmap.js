625b375165a642f3a6b95b9c52491c95ccf8a3ce
26prajval98 <26prajval98@gmail.com>
2020-07-30 23:01:26 +0530

    done

--- client/src/components/covid19/worldmap.js
+++ client/src/components/covid19/worldmap.js
@@ -16,7 +16,7 @@ export default function WorldMap(props) {
 						{content["Country"]} <br />
 						Confirmed : {content["Confirmed"] ? content["Confirmed"] : "-"} <br />
 						Deaths : {content["Deaths"] ? content["Deaths"] : "-"} <br />
-						Recoverd : {content["Recoverd"] ? content["Recoverd"] : "-"} <br />
+						Recovered : {content["Recovered"] ? content["Recoverd"] : "-"} <br />
 						Active : {content["Active"] ? content["Active"] : "-"} <br />
 					</p>
 				}
